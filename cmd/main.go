7999c1d4649fe0c0e290f20bfec7d8f16d0ee4af
CS230832 <230832@centralasian.uz>
2024-04-24 14:15:28 +0500

    Initial Structure of the code

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,42 @@
 package main
 
 import (
+	"CS230832/BeastVehicles/cmd/api"
+	"CS230832/BeastVehicles/config"
+	"CS230832/BeastVehicles/db"
+	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	log.Println("Starting server...")
-
-	server := http.NewServeMux()
-
-	server.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<h1>Hello, World!</h1>")
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: config.Envs.DBUser,
 	})
-	
-	log.Println("Started server on port :8080")
-	if err := http.ListenAndServe("0.0.0.0:8080", server); err != nil {
-		log.Fatal(err)
+
+	if err != nil {
+		panic(err)
 	}
+
+	defer db.Close()
+
+	if err := initStorage(db); err != nil {
+		panic(err)
+	}
+
+	log.Println("Database connected")
+
+	addr := fmt.Sprintf("%s:%s", config.Envs.Host, config.Envs.Port)
+
+	apiServer := api.NewApiServer(addr, db)
+
+	if err := apiServer.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func initStorage(db *sql.DB) error {
+	return db.Ping()
 }
